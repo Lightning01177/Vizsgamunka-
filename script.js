8671103dc1955b91e39b7ef2308ea603042d5b49
Lightning01177 <59297213+Lightning01177@users.noreply.github.com>
2025-12-25 18:03:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// gallery.html
+
+function openLightbox(img) {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
